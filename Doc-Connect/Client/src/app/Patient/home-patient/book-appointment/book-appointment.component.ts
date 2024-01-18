1ca93531976e9990e47e6dd49650cd39697f72bb
Devanshu Chhipani <devanshuchhipani@gmail.com>
2024-01-18 09:28:05 +0530

    feat : doctor appoinment booking calender.

--- Doc-Connect/Client/src/app/Patient/home-patient/book-appointment/book-appointment.component.ts
+++ Doc-Connect/Client/src/app/Patient/home-patient/book-appointment/book-appointment.component.ts
@@ -12,9 +12,73 @@ export class BookAppointmentComponent implements OnInit {
   doctorId?: number;
   doctor: any; // Adjust the type accordingly
 
+  MONTH_NAMES = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
+  DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  showDatepicker = false;
+  datepickerValue!: string;
+  month!: number; // !: mean promis it will not be null, and it will definitely be assigned
+  year!: number;
+  no_of_days = [] as number[];
+  blankdays = [] as number[];
+
+  initDate() {
+    let today = new Date();
+    this.month = today.getMonth();
+    this.year = today.getFullYear();
+    this.datepickerValue = new Date(this.year, this.month, today.getDate()).toDateString();
+  }
+
+  isToday(date: any) {
+    const today = new Date();
+    const d = new Date(this.year, this.month, date);
+    return today.toDateString() === d.toDateString() ? true : false;
+  }
+
+  getDateValue(date: any) {
+    let selectedDate = new Date(this.year, this.month, date);
+    this.datepickerValue = selectedDate.toDateString();
+    this.showDatepicker = false;
+  }
+
+  getNoOfDays() {
+    const daysInMonth = new Date(this.year, this.month + 1, 0).getDate();
+
+    // find where to start calendar day of week
+    let dayOfWeek = new Date(this.year, this.month).getDay();
+    let blankdaysArray = [];
+    for (var i = 1; i <= dayOfWeek; i++) {
+      blankdaysArray.push(i);
+    }
+
+    let daysArray = [];
+    for (var i = 1; i <= daysInMonth; i++) {
+      daysArray.push(i);
+    }
+
+    this.blankdays = blankdaysArray;
+    this.no_of_days = daysArray;
+  }
+
+  trackByIdentity = (index: number, item: any) => item;
+
   constructor(private route: ActivatedRoute,private services : ServicesService) {}
 
   async ngOnInit() : Promise<void> {
+
     try {
       // Read the doctorId from the route parameters
       console.log("hello");
@@ -22,18 +86,20 @@ export class BookAppointmentComponent implements OnInit {
       console.log(params);
       const id = params?.['id']; // Access the correct property name
     
-    if (id !== undefined) {
-      this.doctorId = id;
-      console.log(this.doctorId);
-      // Use the doctorId to fetch the corresponding doctor details
-      // Fetch the data or use a service to get the details based on the id
-      this.doctor = await this.services.getDoctor(this.doctorId).toPromise();
-      
-      console.log("Home Page");
-      console.log(this.doctor);
-    } else {
-        console.error("Error: 'id' parameter is undefined.");
+      if (id !== undefined) {
+        this.doctorId = id;
+        console.log(this.doctorId);
+        // Use the doctorId to fetch the corresponding doctor details
+        // Fetch the data or use a service to get the details based on the id
+        this.doctor = await this.services.getDoctor(this.doctorId).toPromise();
+        
+        console.log("Home Page");
+        console.log(this.doctor);
+      } else {
+          console.error("Error: 'id' parameter is undefined.");
       }
+      this.initDate();
+      this.getNoOfDays(); 
     } catch (error) {
       console.error("error", error);
     }
